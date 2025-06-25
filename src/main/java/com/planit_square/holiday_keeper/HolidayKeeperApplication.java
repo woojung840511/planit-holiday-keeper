b93692ad4513158d8e068e68fe23e4f72974a8d3
woojung8405 <woojung8405@gmail.com>
2025-06-25 23:23:37 +0900

    Initial commit : project setup

---
+++ src/main/java/com/planit_square/holiday_keeper/HolidayKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.planit_square.holiday_keeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayKeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planit_square/holiday_keeper/HolidayKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planit_square.holiday_keeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
